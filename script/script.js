3de7da8ad3ea1231cff42f9dfd2a3a68d2583c38
Morkovin Anton <antonmorkowin@yandex.ru>
2023-03-09 18:38:56 +0300

    .

--- script/script.js
+++ script/script.js
@@ -1,46 +1,31 @@
 const buttonOpen = document.getElementById('button-edit');
-buttonOpen.addEventListener('click', function(event){
-    const popup = document.getElementById('popup');
-    popup.classList.remove('popup_hidden');
-
-    let profileName = document.getElementById('name').textContent;
-    let workplace = document.getElementById('workplace').textContent;
-
-    const inputName = document.getElementById('name-edit');
-    const inputWorkplace = document.getElementById('workplace-edit');
-    inputName.setAttribute('placeholder', profileName);
-    inputWorkplace.setAttribute('placeholder', workplace);
-
-    inputName.addEventListener('input', function(event){
-        const valueName = inputName.value;
-        const name = document.getElementById('name');
-        const saveButton = document.getElementById('save-button');
-
-        saveButton.addEventListener('click', function(event){
-            name.textContent = valueName;
-            inputName.value = '';
-            popup.classList.add('popup_hidden');
-        })
-    })
-
-    inputWorkplace.addEventListener('input', function(event){
-        const valueStatus = inputWorkplace.value;
-        const status = document.getElementById('workplace');
-        const saveButton = document.getElementById('save-button');
-
-        saveButton.addEventListener('click', function(event){
-            status.textContent = valueStatus;
-            inputWorkplace.value = '';
-            popup.classList.add('popup_hidden');
-        })
-
-
-    })
-    
-})
-
 const buttonClose = document.getElementById('close-button');
-buttonClose.addEventListener('click', function(event){
-    const popup = document.getElementById('popup');
-    popup.classList.add('popup_hidden');
-})
+const popup = document.getElementById('popup');
+const inputName = document.getElementById('name-edit');
+const inputWorkplace = document.getElementById('workplace-edit');
+const saveButton = document.getElementById('save-button');
+const name = document.getElementById('name');
+const status = document.getElementById('workplace'); 
+const form = document.getElementById('form');
+
+function OpenEditForm(){
+    popup.classList.add('popup_opened');
+    inputName.value = name.textContent;
+    inputWorkplace.value = status.textContent;
+}
+
+function CloseEditForm(){
+    popup.classList.remove('popup_opened');
+}
+
+function ProfileEdit(){
+    name.textContent = inputName.value;
+    status.textContent = inputWorkplace.value;
+    CloseEditForm();
+}
+
+buttonOpen.addEventListener('click', OpenEditForm);
+
+form.addEventListener('submit', ProfileEdit);   
+ 
+buttonClose.addEventListener('click', CloseEditForm);
